9a86ac233641895ed893bdc940d6faccbdb28bdd
Tim Chen <timchen0326ca@gmail.com>
2025-05-10 15:35:02 -0400

    first commit

---
+++ src/components/Contact/index.js
@@ -0,0 +1,110 @@
+import { useEffect, useState, useRef } from 'react'
+import Loader from 'react-loaders'
+import 'leaflet/dist/leaflet.css'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import emailjs from '@emailjs/browser'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+import L from 'leaflet'
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+
+delete L.Icon.Default.prototype._getIconUrl
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+})
+
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const form = useRef()
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+    return () => clearTimeout(timeoutId)
+  }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm('service_9zmvmm8', 'template_0d9lvno', form.current, 'ldpvuQWFWE9-Ov2Ph')
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send the message, please try again')
+        }
+      )
+  }
+
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C','o','n','t','a','c','t',' ','m','e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I am interested in freelance opportunities—especially on ambitious
+            or large projects. However, if you have any other requests or
+            questions, don't hesitate to contact me using the form below.
+          </p>
+          <div className="contact-form">
+            <form ref={form} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input placeholder="Name" type="text" name="name" required />
+                </li>
+                <li className="half">
+                  <input placeholder="Email" type="email" name="email" required />
+                </li>
+                <li>
+                  <input placeholder="Subject" type="text" name="subject" required />
+                </li>
+                <li>
+                  <textarea placeholder="Message" name="message" required />
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+        <div className="info-map">
+          Tim Chen,
+          <br />
+          Toronto, ON,
+          <br />
+          Canada
+          <br />
+          <br />
+          <span>timchen0326ca@gmail.com</span>
+        </div>
+        <div className="map-wrap">
+          <MapContainer center={[43.6532, -79.3832]} zoom={13}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[43.6532, -79.3832]}>
+              <Popup>Tim lives here, come over for a cup of coffee</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Contact
