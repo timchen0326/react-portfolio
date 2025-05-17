fdca1b7483828f0cfb548efb7fbddd2358603a09
Tim Chen <timchen0326ca@gmail.com>
2025-05-16 20:03:04 -0400

    portfolio: update portfolio images and data

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Loader from "react-loaders";
 import './index.scss';
 import AnimatedLetters from "../AnimatedLetters";
@@ -6,6 +6,35 @@ import portfolioData from '../../data/portfolio.json';
 
 const Portfolio = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 3000);
+
+        return () => {
+            clearTimeout(timeout);
+        }
+    });
+
+    const renderPortfolio = (portfolio) => {
+        return (
+            <div className="images-container">
+                {portfolio.map((port, index) => {
+                    return (
+                        <div className="image-box" key={index}>
+                            <img src={process.env.PUBLIC_URL + '/' + port.cover} className="portfolio-image" alt="portfolio" />
+                            <div className="content">
+                                <p className="title">{port.title}</p>
+                                <h4 className="description">{port.description}</h4>
+                                <button className="btn" onClick={() => window.open(port.link)}>View</button>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        )   
+    }
     return (
         <>
             <div className="container portfolio-page">
@@ -16,7 +45,7 @@ const Portfolio = () => {
                         idx={15}
                     />
                 </h1>
-                {/* <div>{renderPortfolio()}</div> */}
+                <div>{renderPortfolio(portfolioData.portfolio)}</div>
             </div>
             <Loader type="pacman" />
         </>
